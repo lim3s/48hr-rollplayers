07f5dec3deef01f5cb2c2eec10eaa83a42ef1f7d
Lauraducky <laura-h-95@live.com.au>
2015-10-03 16:40:48 +1000

    Updated Background music implementation

--- Assets/Scripts/Global/BackgroundMusic.cs
+++ Assets/Scripts/Global/BackgroundMusic.cs
@@ -3,25 +3,48 @@ using System.Collections;
 
 [RequireComponent(typeof(AudioSource))]
 public class BackgroundMusic : MonoBehaviour {
-	public AudioClip bgMusic;
-	AudioSource[] levelMusic;
+	public AudioClip level1;
+	public AudioClip level2;
+	public AudioClip level2b;
+	public AudioClip level3;
+	public AudioClip level3b;
+
+	AudioSource levelMusic;
 	float volume = 0.7f;
 	int playing = 0;
+	bool part2 = false;
 
 	// Use this for initialization
 	void Start () {
-		levelMusic = GetComponents<AudioSource>();
-		for (int i=0; i<levelMusic.Length; i++) {
-			levelMusic[i].volume = volume;
-		}
-		levelMusic [playing].Play ();
+		levelMusic = GetComponent<AudioSource>();
+		levelMusic.loop = true;
+		startMusic (0, 0);
 	}
 
-	public void startMusic(int i){
-		if (i >= 0 && i < levelMusic.Length) {
-			levelMusic [playing].Stop ();
-			levelMusic [i].Play ();
-			playing = i;
+	public void startMusic(int level, int stage){
+		float time = 0;
+		levelMusic.Stop ();
+		switch (level) {
+		case 0:
+			levelMusic.clip = level1;
+			break;
+		case 1:
+			if(stage == 0){
+				levelMusic.clip = level2;
+			} else if (stage == 1){
+				time = levelMusic.time;
+				levelMusic.clip = level2b;
+			}
+			break;
+		case 2:
+			if (stage == 0) {
+				levelMusic.clip = level3;
+			} else if (stage == 1){
+				levelMusic.clip = level3b;
+			}
+			break;
 		}
+		levelMusic.Play ();
+		levelMusic.time = time;
 	}
 }
