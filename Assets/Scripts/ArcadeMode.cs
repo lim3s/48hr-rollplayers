6a12d2a751f4a1c03a3a39367044c78d17ef54ed
Ben Hill <benhill.limes@gmail.com>
2015-10-04 09:48:11 +1000

    aeiou

---
+++ Assets/Scripts/ArcadeMode.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArcadeMode : MonoBehaviour {
+	[SerializeField]
+	BoxCollider2D mapBounds;
+	[SerializeField]
+	GameObject goal;
+
+	float playerPoints;
+	GameObject currGoal;
+	// Use this for initialization
+	void Start () {
+		currGoal = (GameObject)Instantiate (goal, mapBounds.bounds.center, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
