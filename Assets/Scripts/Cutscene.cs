0ce5b88b18cd4349102dab77ade4355000ae4061
Evelyne Berger-Uichanco <evelyne.rose@ymail.com>
2015-10-04 06:09:24 +1000

    Added intro cutscene

---
+++ Assets/Scripts/Cutscene.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cutscene : MonoBehaviour {
+
+	public Sprite[] sprites;
+	SpriteRenderer myRenderer;
+	AudioSource myAudio;
+	int count = 0;
+	float timer;
+	float length = 3;
+
+	// Use this for initialization
+	void Start () {
+		myRenderer = GetComponent<SpriteRenderer> ();
+		myAudio = GetComponent<AudioSource> ();
+		myAudio.Play ();
+		myRenderer.sprite = sprites [count];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if (timer > length) {
+			timer = 0;
+			count++;
+			if(count < sprites.Length){
+				myRenderer.sprite = sprites[count];
+			} else if (count >= sprites.Length){
+				Application.LoadLevel(Application.loadedLevel + 1);
+			}
+		}
+	}
+}
