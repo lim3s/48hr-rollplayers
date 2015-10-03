509bca67ddfcac14fffd24aefcb2728226158a95
Ben Hill <benhill.limes@gmail.com>
2015-10-04 03:14:11 +1000

    PUSH PUSH!

---
+++ Assets/Scripts/ParallaxBackground.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxBackground : MonoBehaviour {
+	[SerializeField]
+	Camera cam;
+	[SerializeField]
+	float parallaxRatio = 0.5f;
+	Vector2 lastCamPos;
+	// Use this for initialization
+	void Start () {
+		lastCamPos = cam.transform.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector2 curr = cam.transform.position;
+		Vector2 diff = lastCamPos - curr;
+		print (diff);
+		transform.position = new Vector2 (transform.position.x + diff.x * parallaxRatio, transform.position.y);
+		lastCamPos = curr;
+	}
+}
