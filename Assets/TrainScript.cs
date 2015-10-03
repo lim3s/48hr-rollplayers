94499108e10be6174184a209c599aba193fb27be
Callum Grier <cal.grier@gmail.com>
2015-10-04 04:59:46 +1000

    no message

---
+++ Assets/TrainScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrainScript : MonoBehaviour {
+
+	[SerializeField]
+	float speed = 50;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.position += transform.right * speed * Time.fixedDeltaTime;
+	}
+}
