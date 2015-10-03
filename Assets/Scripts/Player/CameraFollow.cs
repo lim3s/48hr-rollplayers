8db44f7c9413a3dd3c7c39ae166cab40cec69aa8
Ben Hill <benhill.limes@gmail.com>
2015-10-03 20:47:53 +1000

    camera bounds

--- Assets/Scripts/Player/CameraFollow.cs
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -6,7 +6,8 @@ public class CameraFollow : MonoBehaviour {
 	public Transform player;
 	FlightController flight;
 	Camera cam;
-
+	[SerializeField]
+	BoxCollider2D bounds;
 	[SerializeField]
 	float speed = 0.1f;
 	float distance = 10;
@@ -15,12 +16,28 @@ public class CameraFollow : MonoBehaviour {
 	float minZoom = 20;
 	float maxZoom = 60;
 
+	float halfHeight;
+	float halfWidth;
+
+	float minX;
+	float minY;
+	float maxX;
+	float maxY;
+
 	public bool stall = true;
 
 	// Use this for initialization
 	void Start () {
 		flight = player.GetComponent<FlightController> ();
 		cam = GetComponent<Camera> ();
+
+		halfHeight = cam.orthographicSize;
+		halfWidth = halfHeight * cam.aspect;
+		minX = bounds.bounds.min.x;
+		minY = bounds.bounds.min.y;
+		maxX = bounds.bounds.max.x;
+		maxY = bounds.bounds.max.y;
+
 	}
 	
 	// Update is called once per frame
@@ -34,8 +51,15 @@ public class CameraFollow : MonoBehaviour {
 		else if (newSize < minZoom)
 			newSize = minZoom;
 		cam.orthographicSize = Mathf.Lerp (cam.orthographicSize, newSize, 0.05f);
+		halfHeight = cam.orthographicSize;
+		halfWidth = halfHeight * cam.aspect;
+		minX = bounds.bounds.min.x + halfWidth;
+		minY = bounds.bounds.min.y + halfHeight;
+		maxX = bounds.bounds.max.x - halfWidth;
+		maxY = bounds.bounds.max.y - halfHeight;
 
 		Vector3 newpos = new Vector3 (player.position.x, player.position.y, -10);
 		transform.position = Vector3.Lerp (transform.position, newpos, speed);
+		transform.position = new Vector3(Mathf.Clamp(transform.position.x, minX, maxX), Mathf.Clamp(transform.position.y, minY, maxY), -10);
 	}
 }
