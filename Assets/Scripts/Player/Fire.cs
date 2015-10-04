d00002ff86a2d3db03cbe4eea680e17b5e1b8fb7
Lauraducky <laura-h-95@live.com.au>
2015-10-04 12:32:39 +1000

    Lit a fire under the player's butt (sometimes). Fixed poop (again). Fixed player prefab

---
+++ Assets/Scripts/Player/Fire.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fire : MonoBehaviour {
+
+	FlightController flight;
+	SpriteRenderer myRenderer;
+
+	// Use this for initialization
+	void Start () {
+		flight = GetComponentInParent<FlightController> ();
+		myRenderer = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (flight.superPowered && Input.GetAxis ("Flap") > 0.8) {
+			myRenderer.enabled = true;
+		} else {
+			myRenderer.enabled = false;
+		}
+	}
+}
