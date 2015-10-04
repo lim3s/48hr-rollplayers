a636196b06c1d85357fc7a05066b2e7ccced0b20
Evelyne Berger-Uichanco <evelyne.rose@ymail.com>
2015-10-04 10:26:48 +1000

    Clouds on level 2

---
+++ Assets/Cloud.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cloud : MonoBehaviour {
+
+	CloudController main;
+	float speed = 1f;
+	float velocity = 5;
+
+	// Use this for initialization
+	void Start () {
+		main = this.GetComponentInParent<CloudController> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector2 newPos = new Vector2 (transform.position.x + velocity, transform.position.y);
+		transform.position = Vector2.Lerp (transform.position, newPos, Time.fixedDeltaTime * speed);
+
+		if (transform.position.x >= main.finish.position.x) {
+			transform.position = new Vector2 (main.start.position.x, transform.position.y);
+		}
+	}
+}
